f550faae595f65ae9a02ed12f32dac63e5306337
ollesu <mkaysa@gmail.com>
2020-08-03 17:22:35 +0100

    add higher base thna 10

--- decimal_to_any_base.go
+++ decimal_to_any_base.go
@@ -1,11 +1,19 @@
 package algorithms
 
+/*
+	bases above 10 are represented with letters (eg A, B, C - think of hex numbers)
+
+
+*/
+
 func convertDecToBase(dec int, base int) string {
+
+	const charset = "0123456789ABCDEF"
 	var result string
 	for dec > 0 {
 		remainder := dec % base
 		dec = dec / base
-		result = string(remainder) + result
+		result = string(charset[remainder]) + result // if base is >= 10, use letters
 	}
 
 	return result
